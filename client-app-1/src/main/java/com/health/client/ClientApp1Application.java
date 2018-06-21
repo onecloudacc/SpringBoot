414184fadf15f76ce1b5a60b4e00d7fcf8c61741
rajeshmahadevan <malathirajesh08092014@gmail.com>
2018-06-21 19:32:19 +0530

    Pushing springboot client1 app

---
+++ client-app-1/src/main/java/com/health/client/ClientApp1Application.java
@@ -0,0 +1,12 @@
+package com.health.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApp1Application.class, args);
+	}
+}

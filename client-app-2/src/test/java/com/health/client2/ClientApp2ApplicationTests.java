72e4d56f6e8828a8c8568509f72b642762445ceb
rajeshmahadevan <malathirajesh08092014@gmail.com>
2018-06-21 19:36:08 +0530

    Pushing springboot client2 api

---
+++ client-app-2/src/test/java/com/health/client2/ClientApp2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.health.client2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientApp2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

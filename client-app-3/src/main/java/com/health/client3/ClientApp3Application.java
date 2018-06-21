9e4055b5a41541975dcbac8514cc71ee7979508e
rajeshmahadevan <malathirajesh08092014@gmail.com>
2018-06-21 19:39:19 +0530

    Pushing springboot client 3 api

---
+++ client-app-3/src/main/java/com/health/client3/ClientApp3Application.java
@@ -0,0 +1,12 @@
+package com.health.client3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApp3Application.class, args);
+	}
+}
---
+++ client-app-3/src/test/java/com/health/client3/ClientApp3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.health.client3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientApp3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
